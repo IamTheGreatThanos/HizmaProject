1020f2d6433e9e6899a8172d6538bd6b1dcd323d
MacBook <MacBook@Thanoss-MacBook.local>
2022-03-10 14:35:46 +0600

    Creating...

---
+++ lib/views/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hizma_project/components/app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          appBar: PreferredSize(preferredSize: const Size.fromHeight(60),
+          child: buildAppBar('asdf')),
+          body: Image.asset('assets/images/logo.png'),
+        ));
+  }
+}
